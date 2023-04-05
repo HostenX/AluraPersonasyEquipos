b258d4815e7909a80b06972819edcd38ed99a803
HostenX <jear1010@outlook.com>
2023-04-05 15:59:17 -0500

    Finalizada clase 3

---
+++ src/components/OptionList/index.js
@@ -0,0 +1,18 @@
+import './optionList.css'
+
+const OptionList = () =>{
+    return <div>
+        <label>Equipos</label>
+        <select>
+            <option>Programación</option>
+            <option>Front End</option>
+            <option>Data Sience</option>
+            <option>DevOps</option>
+            <option>Ux y Diseño</option>
+            <option>Móvil</option>
+            <option>Innovación y Gestión</option>
+        </select>
+    </div>
+}
+
+export default OptionList;
