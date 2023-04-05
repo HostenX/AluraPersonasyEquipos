37241903d5dc4efdaee445b6ab64f166064ee540
HostenX <jear1010@outlook.com>
2023-04-05 10:05:03 -0500

    Adelantado hasta la clase 3 del curso de React Desarrollando con JavaScrip

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Form from './components/form/Form';
+console.log(Header);
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
